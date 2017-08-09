31b5493c7dbe5ebbbd1b69f8c979a94a7769ecdb
zhaodazzle <zhaodazzle@gmail.com>
2017-08-09 23:21:42 +0800

    leetcode solution 14

---
+++ src/com/leetcode/problems/Solution14.java
@@ -0,0 +1,33 @@
+package com.leetcode.problems;
+
+/**
+ * Created by zhaoz on 2017/8/9.<br/>
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ */
+public class Solution14 {
+
+    public static void main(String[] args) {
+        String [] strs = {"abcdef", "abcefd", "abcedf", "abcd"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    private static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length ==0) return "";
+        if (strs.length ==1) return strs[0];
+        int length = Integer.MAX_VALUE;
+        for (String str : strs) {
+            length = (str.length() < length) ? str.length() : length;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            for (String str : strs) {
+                if (str.charAt(i) != strs[0].charAt(i)) {
+                    return sb.toString();
+                }
+            }
+            sb.append(strs[0].charAt(i));
+        }
+        return sb.toString();
+    }
+
+}
